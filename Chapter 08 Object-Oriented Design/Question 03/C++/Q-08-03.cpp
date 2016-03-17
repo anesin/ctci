4c249211d30337442acdfcf4deca379cf0c56930
anesin <anesiner@gmail.com>
2016-03-17 16:49:57 +0900

    [C++] 8.3 Design a musical jukebox using object-oriented principles.

--- Chapter 08 Object-Oriented Design/Question 03/C++/Q-08-03.cpp
+++ Chapter 08 Object-Oriented Design/Question 03/C++/Q-08-03.cpp
@@ -7,6 +7,8 @@
 #include <queue>
 #include <string>
 #include <iostream>
+#include <thread>
+#include <mutex>
 
 using namespace std;
 
@@ -19,10 +21,53 @@ typedef struct {
 
 
 class Player {
+private:
+	queue<const Music *> play_list_;
+	bool playing_;
+
+	recursive_mutex mutex_;
+
 public:
-	void play(const Music *music) const {
-		cout << "Title: " << music->artist << endl;
-		cout << "Artist: " << music->title << endl;
+	Player() : playing_(false) {}
+
+	void clear() {
+		lock_guard<recursive_mutex> lock(mutex_);
+		playing_ = false;
+		queue<const Music *> trash;
+		play_list_.swap(trash);
+	}
+
+	void ready(const Music *music) {
+		lock_guard<recursive_mutex> lock(mutex_);
+		if (music)
+			play_list_.push(music);
+	}
+
+	void play_all() {
+		lock_guard<recursive_mutex> lock(mutex_);
+		if (playing_ || play_list_.empty())
+			return;
+
+		char log[1024];
+		_snprintf(log, sizeof(log), "[PLAY_ALL] size: %d\n", play_list_.size());
+		cout << log;
+
+		playing_ = true;
+		while (playing_ && play_list_.empty() == false) {
+			const Music *music = play_list_.front();
+			play_list_.pop();
+			if (music)
+				play(*music);
+		}
+		playing_ = false;
+	}
+
+private:
+	void play(const Music &music) {
+		lock_guard<recursive_mutex> lock(mutex_);
+		char log[1024];
+		_snprintf(log, sizeof(log), "\"%d\", %d\n", music.title, music.artist);
+		cout << log;
 	}
 };
 
@@ -30,40 +75,30 @@ public:
 class Jukebox {
 private:
 	vector<Music> music_list_;
-	queue<const Music *> play_list_;
 	Player player_;
-	bool playing_;
 
 public:
-	Jukebox() : playing_(false) {}
-
 	void update(vector<Music> &music_list) {
 		music_list_.swap(music_list);
+		player_.clear();
 	}
 
 	bool play(unsigned int index) {
 		if (index >= music_list_.size())
 			return false;
-		cout << "ready " << index << "th" << endl;
-		play_list_.push(&music_list_[index]);
-		if (playing_ == false)
-			play();
+
+		char log[1024];
+		_snprintf(log, sizeof(log), "ready %dth\n", index);
+		cout << log;
+
+		player_.ready(&music_list_[index]);
+		thread t(&Player::play_all, &player_);
+		t.join();
 		return true;
 	}
 
 	const vector<Music> & music_list() {
 		return music_list_;
-	}
-
-private:
-	void play() {
-		playing_ = true;
-		while (play_list_.empty() == false) {
-			const Music *music = play_list_.front();
-			play_list_.pop();
-			player_.play(music);
-		}
-		playing_ = false;
 	}
 };
 
@@ -82,8 +117,14 @@ int _tmain(int argc, _TCHAR* argv[])
 	Jukebox jukebox;
 	jukebox.update(music_list);
 
-	for (int j = 0; j < 20; ++j)
- 		jukebox.play(j * 2);
+	const static int kTestcase = 20;
+	thread t[kTestcase];
+
+	for (int j = 0; j < kTestcase; ++j)
+		t[j] = thread(&Jukebox::play, &jukebox, j * 2);
+
+	for (int k = 0; k < kTestcase; ++k)
+		t[k].join();
 
 	return 0;
 }
