6cc8ecb07ea9d923049574e0b6981c5b4aa033dd
anesin <anesiner@gmail.com>
2016-04-15 16:38:26 +0900

    [C++] [START] 8.7 Explain how you would design a chat server. In particular, provide details about the various backend components, classes, and methods. What would be the hardest problems to solve?

---
+++ Chapter 08 Object-Oriented Design/Question 07/C++/Q-08-07.cpp
@@ -0,0 +1,14 @@
+// Q-08-07.cpp
+//    Explain how you would design a chat server.
+//    In particular, provide details about the various backend components, classes, and methods.
+//    What would be the hardest problems to solve?
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
