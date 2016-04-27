73df946aef19f464b529395d9ab1479efa5e2fcc
anesin <anesiner@gmail.com>
2016-04-27 12:46:50 +0900

    [C++] [START] 8.8 Othello is played as follows: Each Othello piece is white on one side and black on the other. When a piece is surrounded by its opponents on both the left and right sides, or both the top and bottom, it is said to be captured and its color is flipped. On your turn, you must capture at least one of your opponent's pieces. The game ends when either user has no more valid moves. The win is assigned to the person with the most pieces. Implement the object-oriented design for Othello.

---
+++ Chapter 08 Object-Oriented Design/Question 08/Q-08-08/Q-08-08.cpp
@@ -0,0 +1,18 @@
+// Q-08-08.cpp
+//    Othello is played as follows: Each Othello piece is white on one side and black on the other.
+//    When a piece is surrounded by its opponents on both the left and right sides, or both the top and bottom,
+//    it is said to be captured and its color is flipped.
+//    On your turn, you must capture at least one of your opponent's pieces.
+//    The game ends when either user has no more valid moves.
+//    The win is assigned to the person with the most pieces.
+//    Implement the object-oriented design for Othello.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
