ae132aa98b035bfa9214e8790b45306a7d533755
anesin <anesiner@gmail.com>
2016-06-17 15:49:58 +0900

    [C++] [READY] 9.9 Write an algorithm to print all ways of arranging eight queens on an 8x8 chess board so that none of them share the same row, column or diagonal. In this case, "diagonal" means all diagonals, not just the two that bisect the board.

---
+++ Chapter 09 Recursion and Dynamic Programming/Question 09/C++/Q-09-09.cpp
@@ -0,0 +1,14 @@
+// Q-09-09.cpp
+//   Write an algorithm to print all ways of arranging eight queens on an 8x8 chess board
+//   so that none of them share the same row, column or diagonal.
+//   In this case, "diagonal" means all diagonals, not just the two that bisect the board.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
