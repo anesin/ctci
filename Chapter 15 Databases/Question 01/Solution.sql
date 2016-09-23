4a55896f14a5812f566def203c4e00078db3e332
anesin <anesiner@gmail.com>
2016-09-23 16:53:37 +0900

    [SQLite] 15.1 Write a SQL query to get a list of tenants who are renting more than one apartment.

---
+++ Chapter 15 Databases/Question 01/Solution.sql
@@ -0,0 +1,6 @@
+﻿SELECT TenantName FROM Tenants
+INNER JOIN (
+    SELECT TenantID FROM AptTenants
+    GROUP BY TenantID
+    HAVING COUNT(TenantID) > 1
+) TenantIDs ON Tenants.TenantID = TenantIDs.TenantID;
