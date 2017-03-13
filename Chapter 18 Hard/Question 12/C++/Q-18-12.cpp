76155f3a8e4aa7b009b50d67e1c48b28885a8da4
anesin <anesiner@gmail.com>
2017-03-13 17:35:29 +0900

    [C++][FIX] 18.12 Given an NxN matrix of positive and negative integers, write code to find the submatrix with the largest possible sum.

--- Chapter 18 Hard/Question 12/C++/Q-18-12.cpp
+++ Chapter 18 Hard/Question 12/C++/Q-18-12.cpp
@@ -15,13 +15,15 @@ using namespace std;
 template<size_t N>
 class Matrix
 {
+private:
+	const static size_t kSigmaN = N*(N + 1)/2;
+
 public:
 	Matrix() {
 		unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 		engine_ = default_random_engine(seed);
 		static const int kBound = N*2;
 		distribution_ = uniform_int_distribution<int>(-kBound, kBound);
-		Shuffle();
 	}
 
 	int get(size_t row, size_t col) {
@@ -38,73 +40,66 @@ public:
 		}
 	}
 
-	int Submatrix(size_t &start_r, size_t &start_c,
-	              size_t &finish_r, size_t &finish_c) {
-		InitSums();
+	// time complexity: O(N^4)
+	// space complexity: O(N^3)
+	virtual int Submatrix(size_t &start_r, size_t &start_c,
+	                      size_t &finish_r, size_t &finish_c) {
+		int row_sums[N][kSigmaN];
+		for (size_t i = 0; i < N; ++i) {
+			for (size_t j = 0; j < N; ++j) {
+				int start = j*N - (j - 1)*j/2;
+				row_sums[i][start] = get(i, j);
+				for (size_t k = j + 1; k < N; ++k) {
+					int index = start + k - j;
+					row_sums[i][index] = row_sums[i][index - 1] + get(i, k);
+				}
+			}
+		}
 
 		int sum = get(0, 0);
 		start_r = start_c = finish_r = finish_c = 0;
-
-		auto bigger = [&](size_t r1, size_t c1) {
-			size_t r2, c2;
-			int s = FindMaxSub(r1, c1, r2, c2);
-			if (sum < s) {
+		auto bigger = [&](int col_sum, size_t c1, size_t c2,
+				          size_t r1, size_t r2) {
+			if (sum < col_sum) {
 				start_r = r1;
 				start_c = c1;
 				finish_r = r2;
 				finish_c = c2;
-				sum = s;
+				sum = col_sum;
 			}
 		};
 
-		for (size_t i = 0; i < N; ) {
-			bigger(i, i);
-			for (size_t j = ++i; j < N; ++j) {
-				bigger(i, j);
-				bigger(j, i);
+		size_t c1 = 0;
+		int step_c = N;
+		size_t step_i = step_c;
+		int col_sums[kSigmaN];
+		for (size_t i = 0; i < kSigmaN; ++i) {
+			if (step_i <= i) {
+				++c1;
+				step_i += --step_c;
 			}
-		}
-		return sum;
-	}
+			size_t c2 = c1 + step_c - step_i + i;
 
-private:
-	void InitSums() {
-		for (size_t i = 0; i < N; ++i) {
 			for (size_t j = 0; j < N; ++j) {
 				int start = j*N - (j - 1)*j/2;
-				row_sums_[i][start] = get(i, j);
+				col_sums[start] = row_sums[j][i];
+				bigger(col_sums[start], c1, c2, j, j);
 				for (size_t k = j + 1; k < N; ++k) {
 					int index = start + k - j;
-					row_sums_[i][index] = row_sums_[i][index - 1] + get(i, k);
+					col_sums[index] = col_sums[index - 1] + row_sums[k][i];
+					bigger(col_sums[index], c1, c2, j, k);
 				}
 			}
 		}
-	}
 
-	int FindMaxSub(size_t r1, size_t c1, size_t &r2, size_t &c2) {
-		int s = get(r1, c1);
-		int sum[N] = {0, };
-		for (size_t i = r1; i < N; ++i) {
-			for (size_t j = c1; j < N; ++j) {
-				int index = c1*N - (c1 - 1)*c1/2 + (j - c1);
-				sum[j] += row_sums_[i][index];
-				if (s < sum[j]) {
-					r2 = i;
-					c2 = j;
-					s = sum[j];
-				}
-			}
-		}
-		return s;
+		return sum;
 	}
 
 private:
 	default_random_engine engine_;
 	uniform_int_distribution<int> distribution_;
 	int matrix_[N*N];
-	int row_sums_[N][N*(N + 1)/2];
 };
-
 
 
 int _tmain(int argc, _TCHAR* argv[])
